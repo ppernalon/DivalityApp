638320f4f5201d5483e8dd20dcbd188264039d44
Julie <julie.brunetto@centrale-marseille.fr>
2022-01-06 13:00:40 +0100

    scrollable collection card

--- src/pages/MyCollection/MyCollection.tsx
+++ src/pages/MyCollection/MyCollection.tsx
@@ -1,29 +1,66 @@
 import React, { useState } from "react"
 import {
+    FlatList,
     Image,
+    SafeAreaView,
+    ScrollView,
     View
 } from "react-native"
-import { Text } from "react-native-paper"
+import { Dialog, Text } from "react-native-paper"
 import HeaderTextured from "components/HeaderTextured/HeaderTextured"
 import Disciples from "components/Disciples/Disciples"
 import { colors } from "GlobalStyle"
 import { TouchableOpacity } from "react-native"
+import Card from "components/Card/Card"
+import { myCollectionStyles } from "./MyCollectionStyles"
+import { paperTheme } from "PaperTheme"
+
 
 type MyCollectionProps = {
     navigation: any
 }
+
 
 const MyCollection = ({navigation} : MyCollectionProps) => {
     const [currentPantheon, setCurrentPantheon] = useState<string>("egyptian")
     const [egyptianCards, setEgyptianCards] = useState<string[]>([])
     const [greekCards, setGreekCards] = useState<string[]>([])
     const [nordicCards, setNordicCards] = useState<string[]>([])
+    const [isDialogVisible, setIsDialogVisible] = useState<boolean>(false)
     
     const fontColor: any = {
         egyptian: colors.egyptianYellow,
         nordic: colors.nordicRed,
         greek: colors.greekBlue
     }
+
+    const MockCardCollection=[
+        {
+        name: "anubis",
+        },
+        {
+        name: "bastet",
+        },
+        {
+        name: "horus",
+        },
+        {
+        name:"osiris"
+        },
+        {
+        name:"osiris"
+        },
+        {
+        name: "anubis",
+        },
+        {
+        name: "bastet",
+        },
+        {
+        name: "horus",
+        },
+
+    ]
 
     const nordicLogo: any = require("@images/pantheon-logos/nordic.png")
     const nordicLogoNoColor: any = require("@images/pantheon-logos/nordic-nocolor.png")
@@ -32,8 +69,29 @@ const MyCollection = ({navigation} : MyCollectionProps) => {
     const egyptianLogo: any = require("@images/pantheon-logos/egyptian.png")
     const egyptianLogoNoColor: any = require("@images/pantheon-logos/egyptian-nocolor.png")
 
+
+    const renderItem = ({ item }: any ) => {    
+        return (
+            <TouchableOpacity onPress={() => onClickCard(item)} style={{margin:15}}>
+                <Card
+                   name={item.name}
+                   minimal={true}
+                />
+            </TouchableOpacity>
+        
+        );
+      };
+
+    const onClickCard = ( { name } : any) => {
+        setIsDialogVisible(true)
+    }
+
+    const hideDialog = () => {
+        setIsDialogVisible(false)
+    } 
+
     return (
-        <View>
+        <View style={{height:'100%'}}>
             <HeaderTextured>
                <Text style={{color: 'white', textAlign: 'center'}}> COLLECTION </Text>
             </HeaderTextured>
@@ -55,9 +113,21 @@ const MyCollection = ({navigation} : MyCollectionProps) => {
                 </TouchableOpacity>
 
             </View>
-            <View>
-
-            </View>
+            <SafeAreaView style={myCollectionStyles.cardCollectionContainer}>
+                <FlatList 
+                data={MockCardCollection}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.name}
+                numColumns={2}
+                scrollEnabled= {true}
+            />  
+            </SafeAreaView>
+            <Dialog visible={isDialogVisible} onDismiss={hideDialog}>
+                <Text>
+                    vklvf
+                </Text>
+            </Dialog>
+             
         </View>
     )
 }
---
+++ src/pages/MyCollection/MyCollectionStyles.ts
@@ -0,0 +1,10 @@
+import { colors } from "GlobalStyle"
+import { StyleSheet, StatusBar} from "react-native"
+
+export const myCollectionStyles = StyleSheet.create({
+    cardCollectionContainer: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
