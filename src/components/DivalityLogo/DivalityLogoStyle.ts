4a6f251c2885908666eee40691844f765fca4389
Julie <julie.brunetto@centrale-marseille.fr>
2022-01-06 11:43:46 +0100

    fix bug logo size

--- src/components/DivalityLogo/DivalityLogoStyle.ts
+++ src/components/DivalityLogo/DivalityLogoStyle.ts
@@ -6,6 +6,7 @@ export const divalityLogoStyle = StyleSheet.create({
     },
     logoImage: {
         resizeMode: "contain",
-        width: '100%'
+        width: '100%',
+        height: 150
     }
 })
